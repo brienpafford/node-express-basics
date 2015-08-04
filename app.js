e1fabb48bc873e50d966dcf43f09e754821c028f
Brien Pafford <bcpaff@gmail.com>
2015-08-04 10:51:28 -0500

    Added style stuff and logging with Loggly

--- app.js
+++ app.js
@@ -1,20 +1,65 @@
+// Node Packages
+
 var express = require('express');
+var fs      = require('fs');
+var lessCSS = require('less-middleware');
 
-var routes = require('./routes/index');
-var pizza = require('./routes/pizza');
+var loggly = require('loggly');
 
-var app = express();
+// Variables from project
+
+var routes  = require('./routes/index');
+var pizza   = require('./routes/pizza');
+
+var app     = express();
 
 app.set('view engine', 'ejs');
 app.set('case sensitive routing', true);
 
-app.locals.title = 'aweso.me';
+app.locals.title = 'awesome_stuff';
 
-app.use(function (req, res, next) {
-  // logging at the top
-  console.log('Request at ' + new Date().toISOString());
+app.use(lessCSS('public'));
+
+// app.use(function (req, res, next) {
+//   // logging at the top
+//   console.log('Request at ' + new Date().toISOString());
+//   next();
+// });
+
+// Create a Stream
+
+var logStream = fs.createWriteStream('access.log', {flags: 'a'});
+
+// Execute winston
+
+app.use(function (err, req, res, next) {
+  var client = require('./lib/loggly')('incoming');
+  client.log({
+    ip: req.ip,
+    date: new Date(),
+    url: req.url,
+    status: res.statusCode,
+    method: req.method,
+    err: err
+  });
   next();
+})
+
+app.use(function(err, req, res, next) {
+  var client = require('./lib/loggly')('error');
+  client.log({
+    ip     : req.ip,
+    date   : new Date(),
+    url    : req.url,
+    status : res.statusCode,
+    method : req.method,
+    error  : err
+  });
+  res.status(500).send('[Error message]');
 });
+
+
+
 app.use(express.static('public'));
 
 app.use('/', routes);
@@ -36,3 +81,4 @@ var server = app.listen(3000, function () {
 
   console.log('Example app listening at http://%s:%d', host, port);
 });
+
